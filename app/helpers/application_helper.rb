c837f5c1f6b9781779a21a01855343f0f4590785
Daniel Moreto <dfmoreto@gmail.com>
2016-04-29 02:34:57 -0300

    Added course views translates

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
 
   @links_translate = [:show, :edit, :back, :destroy]
+  @titles_translate = [:show, :edit, :index, :new]
 
   def current_resource_in_singular
     t("activerecord.models.#{controller_name.singularize}.one")
@@ -20,9 +21,20 @@ module ApplicationHelper
     end
   end
 
+  @titles_translate.each do |page|
+    define_method "translate_title_of_#{page}_page" do
+      construct_translate_for_page(page)
+    end
+  end
+
   private
 
     def construct_translate_for_link(action)
       t("page_content.actions.#{action}", resource: "")
+    end
+
+    def construct_translate_for_page(page)
+      return t("page_content.actions.list", { resource: current_resource_in_plural } ) if page == :index
+      t("page_content.actions.#{page}", { resource: current_resource_in_singular } )
     end
 end
