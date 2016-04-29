cc7172d1c10ae235ea8d68a37275161237eb3e93
Daniel Moreto <dfmoreto@gmail.com>
2016-04-29 01:44:13 -0300

    Translates to pt-BR added

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,28 @@
 module ApplicationHelper
+
+  @links_translate = [:show, :edit, :back, :destroy]
+
+  def current_resource_in_singular
+    t("activerecord.models.#{controller_name.singularize}.one")
+  end
+
+  def current_resource_in_plural
+    t("activerecord.models.#{controller_name.singularize}.other")
+  end
+
+  def current_active_record_attribute
+    "activerecord.attributes.#{controller_name.singularize}"
+  end
+
+  @links_translate.each do |link|
+    define_method "translate_#{link}_link" do
+      construct_translate_for_link(link)
+    end
+  end
+
+  private
+
+    def construct_translate_for_link(action)
+      t("page_content.actions.#{action}", resource: "")
+    end
 end
